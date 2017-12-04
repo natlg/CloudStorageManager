2c67449aa1c05d9589a88aa5d8a373aa417eb31e
Natalia <natalia.gulko@yahoo.com>
2017-12-04 11:25:31 +0300

    Added login/signup form validation using jQuery Validation Plugin

--- src/main/webapp/js/userscript.js
+++ src/main/webapp/js/userscript.js
@@ -22,58 +22,114 @@ function logoutClick() {
 function submitLoginForm() {
     console.log("Submit login form");
     var form = document.getElementById("loginform");
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            console.log("XMLHttpRequest answer is ready");
-            console.log("responseText: " + xhttp.responseText);
-            location.href = "indexpage.html";
-            setAuthorized(true);
-            console.log("XMLHttpRequest isLogged: " + isAuthorized());
-        }
-        else {
-            if (this.readyState === 4 && this.status === 401) {
-                console.log("error in XMLHttpRequest, status: " + this.status, ", readyState: " + this.readyState);
-                $('#login-alert').show();
+    // Initialize form validation on the login form.
+    $("#loginform").validate({
+        // Specify validation rules
+        rules: {
+            email: {
+                required: true,
+                // Specify that email should be validated
+                // by the built-in "email" rule
+                email: true
+            },
+            password: {
+                required: true,
+                minlength: 5
             }
+        },
+        // Specify validation error messages
+        messages: {
+            password: {
+                required: "Please provide a password",
+                minlength: "Your password must be at least 5 characters long"
+            },
+            email: "Please enter a valid email address"
+        },
+        submitHandler: function (form) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState === 4 && this.status === 200) {
+                    console.log("XMLHttpRequest answer is ready");
+                    console.log("responseText: " + xhttp.responseText);
+                    location.href = "indexpage.html";
+                    setAuthorized(true);
+                    console.log("XMLHttpRequest isLogged: " + isAuthorized());
+                }
+                else {
+                    if (this.readyState === 4 && this.status === 401) {
+                        console.log("error in XMLHttpRequest, status: " + this.status, ", readyState: " + this.readyState);
+                        $('#login-alert').show();
+                    }
 
+                }
+            };
+
+            var params = "email=" + form.elements["email"].value +
+                "&password=" + form.elements["password"].value;
+            xhttp.open("POST", "http://localhost:8080/loginform", true);
+            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            console.log("send params:");
+            console.log(params);
+            xhttp.send(params);
         }
-    };
-
-    var params = "email=" + form.elements["email"].value +
-        "&password=" + form.elements["password"].value;
-    xhttp.open("POST", "http://localhost:8080/loginform", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    console.log("send params:");
-    console.log(params);
-    xhttp.send(params);
+    });
 }
 
 function submitSignupForm() {
     console.log("Submit sign up form");
     var form = document.getElementById("signupform");
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-            console.log("XMLHttpRequest answer is ready");
-            console.log("responseText: " + xhttp.responseText);
-            location.href = "login.html";
-        }
-        else {
-            if (this.readyState === 4 && this.status === 401) {
-                console.log("error in XMLHttpRequest, status: " + this.status, ", readyState: " + this.readyState);
-                $("#signup-alert").show();
+    $("#signupform").validate({
+        // Specify validation rules
+        rules: {
+            firstname: "required",
+            lastname: "required",
+            email: {
+                required: true,
+                // Specify that email should be validated
+                // by the built-in "email" rule
+                email: true
+            },
+            password: {
+                required: true,
+                minlength: 5
             }
-        }
-    };
+        },
+        // Specify validation error messages
+        messages: {
+            firstname: "Please enter your firstname",
+            lastname: "Please enter your lastname",
+            password: {
+                required: "Please provide a password",
+                minlength: "Your password must be at least 5 characters long"
+            },
+            email: "Please enter a valid email address"
+        },
+        submitHandler: function (form) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState === 4 && this.status === 200) {
+                    console.log("XMLHttpRequest answer is ready");
+                    console.log("responseText: " + xhttp.responseText);
+                    location.href = "login.html";
+                }
+                else {
+                    if (this.readyState === 4 && this.status === 401) {
+                        console.log("error in XMLHttpRequest, status: " + this.status, ", readyState: " + this.readyState);
+                        $("#signup-alert").show();
+                    }
+                }
+            };
 
-    var params = "email=" + form.elements["email"].value +
-        "&firstname=" + form.elements["firstname"].value +
-        "&lastname=" + form.elements["lastname"].value +
-        "&password=" + form.elements["password"].value;
-    xhttp.open("POST", "http://localhost:8080/signup", true);
-    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    console.log("send params:");
-    console.log(params);
-    xhttp.send(params);
+            var params = "email=" + form.elements["email"].value +
+                "&firstname=" + form.elements["firstname"].value +
+                "&lastname=" + form.elements["lastname"].value +
+                "&password=" + form.elements["password"].value;
+            xhttp.open("POST", "http://localhost:8080/signup", true);
+            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            console.log("send params:");
+            console.log(params);
+            xhttp.send(params);
+        }
+    });
+
 }
