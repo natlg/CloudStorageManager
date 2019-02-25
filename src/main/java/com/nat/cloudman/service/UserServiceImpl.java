28f83f38b60d351467f6a16ae08c776a122042b6
Natalia <natalia.gulko@yahoo.com>
2019-02-25 12:30:14 -0800

    Remove role table

--- src/main/java/com/nat/cloudman/service/UserServiceImpl.java
+++ src/main/java/com/nat/cloudman/service/UserServiceImpl.java
@@ -1,15 +1,8 @@
 package com.nat.cloudman.service;
 
-
-import java.util.Arrays;
-import java.util.HashSet;
-
-import com.nat.cloudman.cloud.UserManager;
-import com.nat.cloudman.model.Role;
 import com.nat.cloudman.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.nat.cloudman.repository.RoleRepository;
 import com.nat.cloudman.repository.UserRepository;
 
 @Service("userService")
@@ -17,8 +10,6 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private RoleRepository roleRepository;
 
     @Override
     public User findUserByEmail(String email) {
@@ -29,8 +20,6 @@ public class UserServiceImpl implements UserService {
     public void saveUser(User user) {
         System.out.println("saveUser");
         user.setActive(1);
-        Role userRole = roleRepository.findByRole("ADMIN");
-        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
         userRepository.save(user);
     }
 
